8620e33ae0f792a0d0e5997119f5a8a51be77125
William Crain <willcrain1@gmail.com>
2019-07-13 08:56:12 -0400

    Initial import

---
+++ src/io/itch/deltabreaker/main/MouseInputHandler.java
@@ -0,0 +1,33 @@
+package io.itch.deltabreaker.main;
+
+import org.jnativehook.mouse.NativeMouseEvent;
+import org.jnativehook.mouse.NativeMouseInputListener;
+
+public class MouseInputHandler implements NativeMouseInputListener {
+	
+	@Override
+	public void nativeMouseClicked(NativeMouseEvent nativeEvent) {
+		
+	}
+
+	@Override
+	public void nativeMousePressed(NativeMouseEvent nativeEvent) {
+		
+	}
+
+	@Override
+	public void nativeMouseReleased(NativeMouseEvent nativeEvent) {
+		
+	}
+
+	@Override
+	public void nativeMouseDragged(NativeMouseEvent nativeEvent) {
+		
+	}
+
+	@Override
+	public void nativeMouseMoved(NativeMouseEvent nativeEvent) {
+		
+	}
+
+}
---
+++ src/io/itch/deltabreaker/main/StartupColortap.java
@@ -0,0 +1,9 @@
+package io.itch.deltabreaker.main;
+
+public class StartupColortap {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
